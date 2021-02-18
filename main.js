f86b6fd532ee614fd7fb1f10c6f2ef41161b2dd0
LDN <ledenevdn@gmail.com>
2021-02-18 23:53:38 +0700

    Add list rendering

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
-            inventory: 18
+            inventory: 18,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id: 2234, color: 'green'},
+                {id: 2235, color: 'blue'}
+            ]
         }
     }
 })
